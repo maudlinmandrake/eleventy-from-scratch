c3764f881ffe2adc0a9168f750daa618624c47e4
Jennifer Mikac <jennymikac@gmail.com>
2020-09-03 18:17:59 -0400

    Refactor code

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,19 @@ module.exports = config => {
   // Set directories to pass through to the dist folder
 config.addPassthroughCopy('./src/images/');
 
-// Returns work items, sored by display order
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
+// Returns work items, sorted by display order
+config.addCollection('work', collection => {
+  return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+});
+
+// Returns work items, sorted by display order then filtered by featured
+  config.addCollection('featuredWork', collection => {
+  return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+    x => x.data.featured
+  );
+});
 
   return {
     markdownTemplateEngine: 'njk',
