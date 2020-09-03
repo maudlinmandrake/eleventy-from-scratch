3cf5cb4bcc24f1cde43dbe67310f0ee8528d3eba
Jennifer Mikac <jennymikac@gmail.com>
2020-09-03 18:11:57 -0400

    Sort and display collections

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,13 @@ config.addCollection('work', collection => {
     .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
 });
 
+config.addCollection('featureWord', collection => {
+  return collection
+    .getFilteredByGlob('./src/work/*.md')
+    .sort((a, b) => ( Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1))
+    .filter( x => x.data.featured );
+});
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
