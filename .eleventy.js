8afb618f596a94c364da583036d06edd72f1ebeb
Jennifer Mikac <jennymikac@gmail.com>
2020-09-05 12:18:04 -0400

    Require the date filters we just made

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,38 @@
+// Filters
+
+const dateFilter = require("./src/filters/date-filter.js");
+const w3DateFilter = require("./src/filters/w3-date-filter.js");
+
 module.exports = config => {
   // Set directories to pass through to the dist folder
-config.addPassthroughCopy('./src/images/');
+config.addPassthroughCopy("./src/images/");
 
-const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+const sortByDisplayOrder = require("./src/utils/sort-by-display-order.js");
 
 // Returns work items, sorted by display order
-config.addCollection('work', collection => {
-  return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+config.addCollection("work", collection => {
+  return sortByDisplayOrder(collection.getFilteredByGlob("./src/work/*.md"));
 });
 
 // Returns work items, sorted by display order then filtered by featured
-config.addCollection('featuredWork', collection => {
-  return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+config.addCollection("featuredWork", collection => {
+  return sortByDisplayOrder(collection.getFilteredByGlob("./src/work/*.md")).filter(
     x => x.data.featured
   );
 });
 
 // Returns a collection of blog posts in reverse date order
-config.addCollection('blog', collection => {
-  return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+config.addCollection("blog", collection => {
+  return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
 });
 
   return {
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
-      input: 'src',
-      output: 'dist'
+      input: "src",
+      output: "dist"
     }
   };
 };
